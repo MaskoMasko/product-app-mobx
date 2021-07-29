1e2d8ed1811ef4d2020aa7b9eee6125dd5f85fed
MaskoMasko <74075425+MaskoMasko@users.noreply.github.com>
2021-07-29 14:00:30 +0200

    Update FilterScreen.js

--- screens/FilterScreen.js
+++ screens/FilterScreen.js
@@ -1,11 +1,32 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Text, TextInput, ScrollView } from "react-native";
+import { observer } from "mobx-react";
+import { store } from "../store/productStore";
 
 //filtering options
-function FilterScreen() {
+export const FilterScreen = observer(() => {
+  const [value, setValue] = useState("");
+
+  useEffect(() => {
+    if (value == "") {
+      store.state.filteredItems = [];
+      return;
+    }
+    store.filterItems(value);
+  }, [value]);
+
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Home Screen</Text>
+      <TextInput
+        placeholder="Enter Value"
+        value={value}
+        onChangeText={(e) => setValue(e)}
+      ></TextInput>
+      <ScrollView>
+        {store.state.filteredItems.map((e, i) => {
+          return <Text key={i}>{e}</Text>;
+        })}
+      </ScrollView>
     </View>
   );
-}
+});
