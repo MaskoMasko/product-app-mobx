aeda691509114aed36696743e06a64190b2789cd
MaskoMasko <74075425+MaskoMasko@users.noreply.github.com>
2021-07-29 14:59:40 +0200

    Update FilterScreen.js

--- screens/FilterScreen.js
+++ screens/FilterScreen.js
@@ -1,32 +1,66 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, TextInput, ScrollView } from "react-native";
+import { View, Text, TextInput, ScrollView, CheckBox } from "react-native";
 import { observer } from "mobx-react";
 import { store } from "../store/productStore";
 
 //filtering options
 export const FilterScreen = observer(() => {
   const [value, setValue] = useState("");
+  const [selection, setSelection] = useState(false);
 
   useEffect(() => {
+    if (selection) {
+      if (value == "") {
+        store.state.filteredItemsByName = [];
+        return;
+      }
+      store.filterItemsByName(value);
+    }
     if (value == "") {
-      store.state.filteredItems = [];
+      store.state.filteredItemsByNum = [];
       return;
     }
-    store.filterItems(value);
+    store.filterItemsByNum(value);
   }, [value]);
 
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <TextInput
-        placeholder="Enter Value"
-        value={value}
-        onChangeText={(e) => setValue(e)}
-      ></TextInput>
-      <ScrollView>
-        {store.state.filteredItems.map((e, i) => {
-          return <Text key={i}>{e}</Text>;
-        })}
-      </ScrollView>
+    <View
+      style={{
+        flex: 1,
+        margin: 50,
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <CheckBox value={selection} onValueChange={setSelection} />
+      <Text>Name</Text>
+      {selection ? (
+        <View>
+          <TextInput
+            placeholder="Enter name"
+            value={value}
+            onChangeText={(e) => setValue(e)}
+          ></TextInput>
+          <ScrollView>
+            {store.state.filteredItemsByName.map((e, i) => {
+              return <Text key={i}>{e}</Text>;
+            })}
+          </ScrollView>
+        </View>
+      ) : (
+        <View>
+          <TextInput
+            placeholder="Enter num"
+            value={value}
+            onChangeText={(e) => setValue(e)}
+          ></TextInput>
+          <ScrollView>
+            {store.state.filteredItemsByNum.map((e, i) => {
+              return <Text key={i}>{e}</Text>;
+            })}
+          </ScrollView>
+        </View>
+      )}
     </View>
   );
 });
