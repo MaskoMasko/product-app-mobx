f7d9e1399096593e59e9e758474c03675c9d7729
MaskoMasko <74075425+MaskoMasko@users.noreply.github.com>
2021-07-30 09:36:17 +0200

    Update MainProductScreen.js

--- screens/MainProductScreen.js
+++ screens/MainProductScreen.js
@@ -17,6 +17,14 @@ import { observer } from "mobx-react";
 export const MainProductScreen = observer(({ navigation }) => {
   const [showOptions, setShowOptions] = useState(false);
   const url = "http://mockapi.ddns.net/APIHandler";
+  const scrollRef = useRef();
+
+  const onNextPress = () => {
+    scrollRef.current?.scrollTo({
+      y: 0,
+      animated: false,
+    });
+  };
 
   useEffect(() => {
     store.fetchingData(url);
@@ -53,64 +61,78 @@ export const MainProductScreen = observer(({ navigation }) => {
           }}
         ></CustBtn>
       </Modal>
-      <ScrollView style={{ backgroundColor: "#fff6cc" }}>
-        {store.state.itemsPerPageArray.map((product, idx) => {
-          const { naslov, cijenaUKN, dostupneVelicine, id } = product;
-          var urlNaslov = naslov.replace(/\//g, "").replace(/\’/g, "");
-          // return (
-          //   <View key={idx}>
-          //     <Text>{id}</Text>
-          //     <Text>{naslov}</Text>
-          //     <Text>{cijenaUKN}</Text>
-          //     <Text>{dostupneVelicine}</Text>
-          //     <Image
-          //       source={{
-          //         uri: `http://mockapi.ddns.net/YEE/${urlNaslov}/1.png`,
-          //       }}
-          //       style={{ width: 200, height: 200 }}
-          //     ></Image>
-          //   </View>
-          // );
-          return (
-            <TouchableOpacity
-              activeOpacity={0.5}
-              style={styles.itemPrePageProduct}
-              key={idx}
-              onPress={() => {
-                store.state.chosenProduct = product;
-                navigation.navigate("ProductDetails");
-              }}
-            >
-              <View style={{ alignItems: "center" }}>
-                <Image
-                  source={{
-                    uri: `http://mockapi.ddns.net/YEE/${urlNaslov}/1.png`,
-                  }}
-                  style={styles.altImage}
-                />
-                <Text style={styles.naslovText}>{naslov}</Text>
-                <View style={styles.textUnderHeading}>
-                  <View style={styles.textPlacement}>
-                    <Text style={styles.THICCText}>CIjena:</Text>
-                    <Text style={styles.THICCText}>{cijenaUKN}</Text>
-                  </View>
-                  <View style={styles.textPlacement}>
-                    <Text style={styles.THICCText}>Dostupne Velicine:</Text>
-                    <Text style={styles.THICCText}>{dostupneVelicine}</Text>
+      {store.state.loading ? (
+        <View
+          style={{
+            width: "100%",
+            height: "90%",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Text style={[styles.naslovText, { fontSize: 30 }]}>Loading...</Text>
+        </View>
+      ) : (
+        <ScrollView ref={scrollRef} style={{ backgroundColor: "#fff6cc" }}>
+          {store.state.itemsPerPageArray.map((product, idx) => {
+            const { naslov, cijenaUKN, dostupneVelicine, id } = product;
+            var urlNaslov = naslov.replace(/\//g, "").replace(/\’/g, "");
+            // return (
+            //   <View key={idx}>
+            //     <Text>{id}</Text>
+            //     <Text>{naslov}</Text>
+            //     <Text>{cijenaUKN}</Text>
+            //     <Text>{dostupneVelicine}</Text>
+            //     <Image
+            //       source={{
+            //         uri: `http://mockapi.ddns.net/YEE/${urlNaslov}/1.png`,
+            //       }}
+            //       style={{ width: 200, height: 200 }}
+            //     ></Image>
+            //   </View>
+            // );
+            return (
+              <TouchableOpacity
+                activeOpacity={0.5}
+                style={styles.itemPrePageProduct}
+                key={idx}
+                onPress={() => {
+                  store.state.chosenProduct = product;
+                  navigation.navigate("ProductDetails");
+                }}
+              >
+                <View style={{ alignItems: "center" }}>
+                  <Image
+                    source={{
+                      uri: `http://mockapi.ddns.net/YEE/${urlNaslov}/1.png`,
+                    }}
+                    style={styles.altImage}
+                  />
+                  <Text style={styles.naslovText}>{naslov}</Text>
+                  <View style={styles.textUnderHeading}>
+                    <View style={styles.textPlacement}>
+                      <Text style={styles.THICCText}>CIjena:</Text>
+                      <Text style={styles.THICCText}>{cijenaUKN}</Text>
+                    </View>
+                    <View style={styles.textPlacement}>
+                      <Text style={styles.THICCText}>Dostupne Velicine:</Text>
+                      <Text style={styles.THICCText}>{dostupneVelicine}</Text>
+                    </View>
                   </View>
                 </View>
-              </View>
-            </TouchableOpacity>
-          );
-        })}
+              </TouchableOpacity>
+            );
+          })}
 
-        <Button
-          title="next"
-          onPress={() => {
-            store.state.counter += 20;
-          }}
-        ></Button>
-      </ScrollView>
+          <Button
+            title="next"
+            onPress={() => {
+              store.state.counter += 20;
+              onNextPress();
+            }}
+          ></Button>
+        </ScrollView>
+      )}
     </View>
   );
 });
