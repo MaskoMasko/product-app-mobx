8b1aba7ba8b930d8114ae7c6134fbd559675f477
MaskoMasko <74075425+MaskoMasko@users.noreply.github.com>
2021-07-30 14:52:15 +0200

    Update FilterScreen.js

--- screens/FilterScreen.js
+++ screens/FilterScreen.js
@@ -6,80 +6,32 @@ import {
   ScrollView,
   CheckBox,
   TouchableOpacity,
+  Button,
 } from "react-native";
 import { observer } from "mobx-react";
 import { store } from "../store/productStore";
+import { CustBtn } from "../components/CustBtn";
+import { action } from "mobx";
 
 //filtering options
 export const FilterScreen = observer(({ navigation }) => {
-  const [value, setValue] = useState("");
-  const [selection, setSelection] = useState(false);
-
-  useEffect(() => {
-    if (selection) {
-      if (value == "") {
-        store.state.filteredItemsByName = [];
-        return;
-      }
-      store.filterItemsByName(value);
-    }
-    if (value == "") {
-      store.state.filteredItemsByNum = [];
-      return;
-    }
-    store.filterItemsByNum(value);
-  }, [value]);
-
   return (
-    <View
-      style={{
-        flex: 1,
-        margin: 50,
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <CheckBox value={selection} onValueChange={setSelection} />
-      <Text>Name</Text>
-      {selection ? (
-        <View>
-          <TextInput
-            placeholder="Enter name"
-            value={value}
-            onChangeText={(e) => setValue(e)}
-          ></TextInput>
-          <ScrollView>
-            {store.state.filteredItemsByName.map((product, i) => {
-              const { naslov } = product;
-              return (
-                <TouchableOpacity
-                  style={{ width: 100, height: 100, backgroundColor: "blue" }}
-                  onPress={() => {
-                    store.selectedProduct(e);
-                    navigation.navigate("ProductDetails");
-                  }}
-                  key={i}
-                >
-                  <Text>{naslov}</Text>
-                </TouchableOpacity>
-              );
-            })}
-          </ScrollView>
-        </View>
-      ) : (
-        <View>
-          <TextInput
-            placeholder="Enter num"
-            value={value}
-            onChangeText={(e) => setValue(e)}
-          ></TextInput>
-          <ScrollView>
-            {store.state.filteredItemsByNum.map((e, i) => {
-              return <Text key={i}>{e}</Text>;
-            })}
-          </ScrollView>
-        </View>
-      )}
+    <View>
+      <Text>Filter Options</Text>
+      <CustBtn
+        title="FILTER BY NASLOV"
+        onPress={() => store.filterFunciton(store.state.value, "naslov")}
+      ></CustBtn>
+      <CustBtn
+        title="FILTER BY CIJENA"
+        onPress={() => store.filterFunciton(store.state.value, "cijenaUKN")}
+      ></CustBtn>
+      <TextInput
+        placeholder="SEARCH BY NAME"
+        // value={store.state.value}
+        value={store.state.value}
+        onChangeText={action((e) => (store.state.value = e))}
+      ></TextInput>
     </View>
   );
 });
