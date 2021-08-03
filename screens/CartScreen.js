409bb203149d397b861cf6408ae064aa130ecbb7
MaskoMasko <persic.massimo@gmail.com>
2021-08-03 22:29:39 +0200

    Update CartScreen.js

--- screens/CartScreen.js
+++ screens/CartScreen.js
@@ -6,94 +6,96 @@ import {
   StyleSheet,
   Image,
   TouchableOpacity,
+  Button,
 } from "react-native";
-import { store } from "../store/productStore";
-import { observer } from "mobx-react";
+import { productStore, store } from "../store/productStore";
+// import { observer } from "mobx-react";
 import { CustBtn } from "../components/CustBtn";
+import { observer } from "mobx-react-lite";
 
 //cart
-export const CartScreen = observer(({ navigation }) => {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <View
-        style={{ backgroundColor: "#fff6cc", height: "100%", width: "100%" }}
-      >
-        <Text style={styles.mainText}>Your Cart:</Text>
-        <ScrollView>
-          {store.state.cart.map((item, idx) => {
-            const { id, naslov, cijenaUKN, size } = item;
-            const urlNaslov = naslov.replace(/\//g, "").replace(/\’/g, "");
+// export const CartScreen = observer(({ navigation }) => {
+//   return (
+//     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+//       <View
+//         style={{ backgroundColor: "#fff6cc", height: "100%", width: "100%" }}
+//       >
+//         <Text style={styles.mainText}>Your Cart:</Text>
+//         <ScrollView>
+//           {store.state.cart.map((item, idx) => {
+//             const { id, naslov, cijenaUKN, size } = item;
+//             const urlNaslov = naslov.replace(/\//g, "").replace(/\’/g, "");
 
-            return (
-              <View
-                key={id}
-                style={{
-                  borderColor: "black",
-                  borderWidth: 1,
-                  borderBottomWidth: 0,
-                }}
-              >
-                <View style={{ flexDirection: "row" }}>
-                  <Image
-                    source={{
-                      uri: `http://mockapi.ddns.net/YEE/${urlNaslov}/1.png`,
-                    }}
-                    style={[
-                      styles.altImage,
-                      { width: 100, height: 100, margin: 10, marginBottom: 0 },
-                    ]}
-                  />
-                  <View>
-                    <Text style={[styles.manjiText, { marginTop: 10 }]}>
-                      {naslov}
-                    </Text>
-                    <Text style={styles.manjiText}>{size}</Text>
-                    <Text style={styles.manjiText}>{cijenaUKN}</Text>
-                  </View>
-                </View>
-                <TouchableOpacity
-                  style={{
-                    backgroundColor: "orange",
-                    width: 150,
-                    borderRadius: 10,
-                    margin: 10,
-                  }}
-                  activeOpacity={0.5}
-                  onPress={() => store.removeItemFromCart(idx)}
-                >
-                  <Text
-                    style={{ color: "white", padding: 10, fontWeight: "bold" }}
-                  >
-                    REMOVE ITEM
-                  </Text>
-                </TouchableOpacity>
-              </View>
-            );
-          })}
-        </ScrollView>
-        <Text
-          style={[
-            styles.mainText,
-            {
-              marginBottom: 20,
-              padding: 10,
-              marginTop: 0,
-              borderTopColor: "black",
-              borderTopWidth: 3,
-            },
-          ]}
-        >
-          Sve Ukupno: {store.racun()} Kn
-        </Text>
-        <CustBtn
-          title="GO BACK"
-          onPress={() => navigation.navigate("Home")}
-        ></CustBtn>
-        <Text></Text>
-      </View>
-    </View>
-  );
-});
+//             return (
+//               <View
+//                 key={id}
+//                 style={{
+//                   borderColor: "black",
+//                   borderWidth: 1,
+//                   borderBottomWidth: 0,
+//                 }}
+//               >
+//                 <View style={{ flexDirection: "row" }}>
+//                   <Image
+//                     source={{
+//                       uri: `http://mockapi.ddns.net/YEE/${urlNaslov}/1.png`,
+//                     }}
+//                     style={[
+//                       styles.altImage,
+//                       { width: 100, height: 100, margin: 10, marginBottom: 0 },
+//                     ]}
+//                   />
+//                   <View>
+//                     <Text style={[styles.manjiText, { marginTop: 10 }]}>
+//                       {naslov}
+//                     </Text>
+//                     <Text style={styles.manjiText}>{size}</Text>
+//                     <Text style={styles.manjiText}>{cijenaUKN}</Text>
+//                   </View>
+//                 </View>
+//                 <TouchableOpacity
+//                   style={{
+//                     backgroundColor: "orange",
+//                     width: 150,
+//                     borderRadius: 10,
+//                     margin: 10,
+//                   }}
+//                   activeOpacity={0.5}
+//                   onPress={() => store.removeItemFromCart(idx)}
+//                 >
+//                   <Text
+//                     style={{ color: "white", padding: 10, fontWeight: "bold" }}
+//                   >
+//                     REMOVE ITEM
+//                   </Text>
+//                 </TouchableOpacity>
+//               </View>
+//             );
+//           })}
+//         </ScrollView>
+//         <Text
+//           style={[
+//             styles.mainText,
+//             {
+//               marginBottom: 20,
+//               padding: 10,
+//               marginTop: 0,
+//               borderTopColor: "black",
+//               borderTopWidth: 3,
+//             },
+//           ]}
+//         >
+//           Sve Ukupno: {store.racun()} Kn
+//         </Text>
+//         <CustBtn
+//           title="GO BACK"
+//           onPress={() => navigation.navigate("Home")}
+//         ></CustBtn>
+//         <Text></Text>
+//       </View>
+//     </View>
+//   );
+// });
 
 const styles = StyleSheet.create({
   mainText: {
@@ -111,4 +113,22 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     margin: 2,
   },
+});
+
+export const CartScreen = observer(({ navigation }) => {
+  return (
+    <View>
+      {productStore.cartList.map((e, i) => {
+        return (
+          <View key={i}>
+            <Text>{e.naslov}</Text>
+            <Button
+              onPress={() => productStore.removeProductFromCartList(i)}
+              title="remove"
+            ></Button>
+          </View>
+        );
+      })}
+    </View>
+  );
 });
