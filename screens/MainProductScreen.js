13a0b557319feac5af94002e881c74928130d163
MaskoMasko <persic.massimo@gmail.com>
2021-08-03 22:29:36 +0200

    Update MainProductScreen.js

--- screens/MainProductScreen.js
+++ screens/MainProductScreen.js
@@ -11,200 +11,201 @@ import {
   Alert,
 } from "react-native";
 import { CustBtn } from "../components/CustBtn";
-import { store } from "../store/productStore";
-import { observer } from "mobx-react";
+import { productStore } from "../store/productStore";
+// import { observer } from "mobx-react";
 import { autorun, action } from "mobx";
+import { observer } from "mobx-react-lite";
 
 //then u go in
-export const MainProductScreen = observer(({ navigation }) => {
-  const [showOptions, setShowOptions] = useState(false);
-  const url = "http://mockapi.ddns.net/APIHandler";
-  const scrollRef = useRef();
-  const coutnerRef = useRef(0);
+// export const MainProductScreen = observer(({ navigation }) => {
+//   const [showOptions, setShowOptions] = useState(false);
+//   const url = "http://mockapi.ddns.net/APIHandler";
+//   const scrollRef = useRef();
+//   const coutnerRef = useRef(0);
 
-  const onNextPress = action(() => {
-    scrollRef.current?.scrollTo({
-      y: 0,
-      animated: false,
-    });
-  });
+//   const onNextPress = action(() => {
+//     scrollRef.current?.scrollTo({
+//       y: 0,
+//       animated: false,
+//     });
+//   });
 
-  useEffect(() => {
-    store.fetchingData(url);
-  }, [store.state.counter]);
+//   useEffect(() => {
+//     store.fetchingData(url);
+//   }, [store.state.counter]);
 
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        backgroundColor: "#fff6cc",
-      }}
-    >
-      <CustBtn
-        title="OPTIONS"
-        path="Options"
-        onPress={() => setShowOptions(true)}
-      ></CustBtn>
-      <Modal visible={showOptions} animationType="slide">
-        <View
-          style={{
-            height: "100%",
-            justifyContent: "space-between",
-            backgroundColor: "#fff6cc",
-          }}
-        >
-          <View>
-            <Text></Text>
+//   return (
+//     <View
+//       style={{
+//         flex: 1,
+//         justifyContent: "center",
+//         backgroundColor: "#fff6cc",
+//       }}
+//     >
+//       <CustBtn
+//         title="OPTIONS"
+//         path="Options"
+//         onPress={() => setShowOptions(true)}
+//       ></CustBtn>
+//       <Modal visible={showOptions} animationType="slide">
+//         <View
+//           style={{
+//             height: "100%",
+//             justifyContent: "space-between",
+//             backgroundColor: "#fff6cc",
+//           }}
+//         >
+//           <View>
+//             <Text></Text>
 
-            <CustBtn
-              title="FILTER"
-              onPress={() => {
-                setShowOptions(false);
-                navigation.navigate("Filter");
-              }}
-            ></CustBtn>
-            <CustBtn
-              title="CART"
-              onPress={() => {
-                setShowOptions(false);
-                navigation.navigate("Cart");
-              }}
-            ></CustBtn>
-          </View>
-          <View>
-            <CustBtn
-              title="GO BACK"
-              onPress={() => {
-                setShowOptions(false);
-              }}
-            ></CustBtn>
-            <Text></Text>
-          </View>
-        </View>
-      </Modal>
-      {store.state.loading ? (
-        <View
-          style={{
-            width: "100%",
-            height: "90%",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          <Text style={[styles.naslovText, { fontSize: 30 }]}>Loading...</Text>
-        </View>
-      ) : (
-        <ScrollView ref={scrollRef} style={{ backgroundColor: "#fff6cc" }}>
-          {store.state.itemsPerPageArray.map((product, idx) => {
-            const { naslov, cijenaUKN, dostupneVelicine, id } = product;
-            const urlNaslov = naslov.replace(/\//g, "").replace(/\’/g, "");
-            return (
-              <TouchableOpacity
-                activeOpacity={0.5}
-                style={[
-                  styles.itemPrePageProduct,
-                  { margin: 0, marginBottom: 20 },
-                ]}
-                key={idx}
-                onPress={action(() => {
-                  store.state.pojedinacneVelicine = [];
-                  store.state.chosenProduct = product;
-                  navigation.navigate("ProductDetails");
-                })}
-              >
-                <View style={{ alignItems: "center" }}>
-                  <Image
-                    source={{
-                      uri: `http://mockapi.ddns.net/YEE/${urlNaslov}/1.png`,
-                    }}
-                    style={styles.altImage}
-                  />
-                  <Text style={styles.naslovText}>{naslov}</Text>
-                  <View style={styles.textUnderHeading}>
-                    <View style={styles.textPlacement}>
-                      <Text style={styles.THICCText}>CIjena:</Text>
-                      <Text style={styles.THICCText}>{cijenaUKN}</Text>
-                    </View>
-                    <View style={styles.textPlacement}>
-                      <Text style={styles.THICCText}>Dostupne Velicine:</Text>
-                      <View>
-                        <Text style={styles.THICCText}>{dostupneVelicine}</Text>
-                      </View>
-                      {/* <View style={styles.THICCText}>
-                        {dostupneVelicine.map((e, id) => {
-                          return <Text key={id}>{e}</Text>;
-                        })}
-                      </View> */}
-                    </View>
-                  </View>
-                </View>
-              </TouchableOpacity>
-            );
-          })}
-          <View style={{ flexDirection: "row", alignSelf: "center" }}>
-            <TouchableOpacity
-              activeOpacity={0.5}
-              disabled={store.state.counter == 0}
-              onPress={action(() => {
-                if (store.state.counter > 0 && coutnerRef.current > 0) {
-                  store.state.counter -= 20;
-                  coutnerRef.current -= 1;
-                  onNextPress();
-                }
-              })}
-              style={
-                store.state.counter == 0
-                  ? [
-                      styles.loginButton,
-                      {
-                        width: "30%",
-                        margin: 10,
-                        backgroundColor: "rgba(255, 162, 80, .7)",
-                      },
-                    ]
-                  : [styles.loginButton, { width: "30%", margin: 10 }]
-              }
-            >
-              <Text style={styles.loginButtonText}>PREV PAGE</Text>
-            </TouchableOpacity>
-            <View>
-              <Text
-                style={[
-                  styles.loginButtonText,
-                  {
-                    marginHorizontal: 5,
-                    fontSize: 18,
-                    marginVertical: 10,
-                    padding: 5,
-                    paddingHorizontal: 15,
-                    textAlign: "center",
-                    backgroundColor: "#04080F",
-                    borderRadius: 10,
-                    color: "white",
-                  },
-                ]}
-              >
-                {coutnerRef.current}
-              </Text>
-            </View>
-            <TouchableOpacity
-              activeOpacity={0.5}
-              onPress={action(() => {
-                store.state.counter += 20;
-                coutnerRef.current += 1;
-                onNextPress();
-              })}
-              style={[styles.loginButton, { width: "30%", margin: 10 }]}
-            >
-              <Text style={styles.loginButtonText}>NEXT PAGE</Text>
-            </TouchableOpacity>
-          </View>
-        </ScrollView>
-      )}
-    </View>
-  );
-});
+//             <CustBtn
+//               title="FILTER"
+//               onPress={() => {
+//                 setShowOptions(false);
+//                 navigation.navigate("Filter");
+//               }}
+//             ></CustBtn>
+//             <CustBtn
+//               title="CART"
+//               onPress={() => {
+//                 setShowOptions(false);
+//                 navigation.navigate("Cart");
+//               }}
+//             ></CustBtn>
+//           </View>
+//           <View>
+//             <CustBtn
+//               title="GO BACK"
+//               onPress={() => {
+//                 setShowOptions(false);
+//               }}
+//             ></CustBtn>
+//             <Text></Text>
+//           </View>
+//         </View>
+//       </Modal>
+//       {store.state.loading ? (
+//         <View
+//           style={{
+//             width: "100%",
+//             height: "90%",
+//             alignItems: "center",
+//             justifyContent: "center",
+//           }}
+//         >
+//           <Text style={[styles.naslovText, { fontSize: 30 }]}>Loading...</Text>
+//         </View>
+//       ) : (
+//         <ScrollView ref={scrollRef} style={{ backgroundColor: "#fff6cc" }}>
+//           {store.state.itemsPerPageArray.map((product, idx) => {
+//             const { naslov, cijenaUKN, dostupneVelicine, id } = product;
+//             const urlNaslov = naslov.replace(/\//g, "").replace(/\’/g, "");
+//             return (
+//               <TouchableOpacity
+//                 activeOpacity={0.5}
+//                 style={[
+//                   styles.itemPrePageProduct,
+//                   { margin: 0, marginBottom: 20 },
+//                 ]}
+//                 key={idx}
+//                 onPress={action(() => {
+//                   store.state.pojedinacneVelicine = [];
+//                   store.state.chosenProduct = product;
+//                   navigation.navigate("ProductDetails");
+//                 })}
+//               >
+//                 <View style={{ alignItems: "center" }}>
+//                   <Image
+//                     source={{
+//                       uri: `http://mockapi.ddns.net/YEE/${urlNaslov}/1.png`,
+//                     }}
+//                     style={styles.altImage}
+//                   />
+//                   <Text style={styles.naslovText}>{naslov}</Text>
+//                   <View style={styles.textUnderHeading}>
+//                     <View style={styles.textPlacement}>
+//                       <Text style={styles.THICCText}>CIjena:</Text>
+//                       <Text style={styles.THICCText}>{cijenaUKN}</Text>
+//                     </View>
+//                     <View style={styles.textPlacement}>
+//                       <Text style={styles.THICCText}>Dostupne Velicine:</Text>
+//                       <View>
+//                         <Text style={styles.THICCText}>{dostupneVelicine}</Text>
+//                       </View>
+//                       {/* <View style={styles.THICCText}>
+//                         {dostupneVelicine.map((e, id) => {
+//                           return <Text key={id}>{e}</Text>;
+//                         })}
+//                       </View> */}
+//                     </View>
+//                   </View>
+//                 </View>
+//               </TouchableOpacity>
+//             );
+//           })}
+//           <View style={{ flexDirection: "row", alignSelf: "center" }}>
+//             <TouchableOpacity
+//               activeOpacity={0.5}
+//               disabled={store.state.counter == 0}
+//               onPress={action(() => {
+//                 if (store.state.counter > 0 && coutnerRef.current > 0) {
+//                   store.state.counter -= 20;
+//                   coutnerRef.current -= 1;
+//                   onNextPress();
+//                 }
+//               })}
+//               style={
+//                 store.state.counter == 0
+//                   ? [
+//                       styles.loginButton,
+//                       {
+//                         width: "30%",
+//                         margin: 10,
+//                         backgroundColor: "rgba(255, 162, 80, .7)",
+//                       },
+//                     ]
+//                   : [styles.loginButton, { width: "30%", margin: 10 }]
+//               }
+//             >
+//               <Text style={styles.loginButtonText}>PREV PAGE</Text>
+//             </TouchableOpacity>
+//             <View>
+//               <Text
+//                 style={[
+//                   styles.loginButtonText,
+//                   {
+//                     marginHorizontal: 5,
+//                     fontSize: 18,
+//                     marginVertical: 10,
+//                     padding: 5,
+//                     paddingHorizontal: 15,
+//                     textAlign: "center",
+//                     backgroundColor: "#04080F",
+//                     borderRadius: 10,
+//                     color: "white",
+//                   },
+//                 ]}
+//               >
+//                 {coutnerRef.current}
+//               </Text>
+//             </View>
+//             <TouchableOpacity
+//               activeOpacity={0.5}
+//               onPress={action(() => {
+//                 store.state.counter += 20;
+//                 coutnerRef.current += 1;
+//                 onNextPress();
+//               })}
+//               style={[styles.loginButton, { width: "30%", margin: 10 }]}
+//             >
+//               <Text style={styles.loginButtonText}>NEXT PAGE</Text>
+//             </TouchableOpacity>
+//           </View>
+//         </ScrollView>
+//       )}
+//     </View>
+//   );
+// });
 
 const styles = StyleSheet.create({
   buttons: {
@@ -273,4 +274,29 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     fontSize: 12,
   },
+});
+
+export const MainProductScreen = observer(({ navigation }) => {
+  const url = "http://mockapi.ddns.net/APIHandler";
+  useEffect(() => {
+    productStore.fetchData(url);
+  }, []);
+  return (
+    <ScrollView>
+      {productStore.productList.map((e, i) => {
+        const { id, naslov } = e;
+        return (
+          <TouchableOpacity
+            onPress={() => {
+              productStore.setSelectedProduct(id);
+              navigation.navigate("ProductDetails");
+            }}
+            key={i}
+          >
+            <Text>{naslov}</Text>
+          </TouchableOpacity>
+        );
+      })}
+    </ScrollView>
+  );
 });
